908c7f216c4c04f67463bbb3e4cbb406d3972cb3
w. chorolque <wchorolque@gmail.com>
2013-09-03 11:46:24 -0400

    referencia

--- clase01.c
+++ clase01.c
@@ -8,8 +8,9 @@ int LeerEntero()
     return x;
 }
 
-int Suma(int a, int b)
+int Suma(int &a, int b)
 {
+    a = 3;
     int c;
     return a + b;
 }
@@ -24,8 +25,12 @@ int main()
     int a, b, c;
     a = LeerEntero();
     b = LeerEntero();
+    printf ("\nvalor a = %d", a);
+    printf ("\nvalor b = %d", b);
     c = Suma(a, b);
-    printf ("Suma: %d\n", c);
-    printf ("Resta: %d", Resta(a, b));
+    printf ("\nSuma: %d", c);
+    printf ("\nResta: %d", Resta(a, b));
+    printf ("\nvalor a = %d", a);
+    printf ("\nvalor b = %d", b);
     return 0;
 }
